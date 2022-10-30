bdb04cdcf51801fc6eadfe0b6d3083e188596a03
Levi Mardis <levimardis@gmail.com>
2022-10-30 18:12:58 -0500

    First commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+let frames;
+const spriteWindow = document.getElementById("sprite-window"),
+  spriteBounding = spriteWindow.getBoundingClientRect(),
+  totalFrames = 50;
+const handleScroll = (e) => {
+  //console.log(frames);
+  const movingBounds = spriteWindow.getBoundingClientRect();
+  const currentFrame = Math.floor(totalFrames - movingBounds.bottom / frames);
+  spriteWindow.style.setProperty("--frame", "-" + currentFrame * 450 + "px");
+  console.log(currentFrame);
+};
+
+const toggleScroll = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document.addEventListener("scroll", handleScroll);
+      frames = spriteBounding.bottom / totalFrames;
+    } else {
+      document.removeEventListener("scroll", handleScroll);
+    }
+    console.log(entry.isIntersecting);
+    // Each entry describes an intersection change for one observed
+    // target element:
+    //   entry.boundingClientRect
+    //   entry.intersectionRatio
+    //   entry.intersectionRect
+    //   entry.isIntersecting
+    //   entry.rootBounds
+    //   entry.target
+    //   entry.time
+  });
+};
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+let observer = new IntersectionObserver(toggleScroll, options);
+let target = document.querySelector("#sprite-window");
+observer.observe(target);
+
+const test = document.getElementById("sprite-window");
+test.style.setProperty("--frame", "green");
+
+//document.addEventListener("scroll", handleScroll);
